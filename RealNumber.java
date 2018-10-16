24f5028e426610e3c541f9046d356a4544f2bb9f
Jason Zheng <zzheng3@stuy.edu>
2018-10-15 23:15:35 -0400

    added compareTo

--- RealNumber.java
+++ RealNumber.java
@@ -3,4 +3,13 @@ public class RealNumber {
 	public double getNumber() {
 		return number;
 	}
+	public String compareTo(double otherNumber) {
+		if (number > otherNumber) {
+			return number + " is bigger than " + otherNumber;
+		}
+		if (number < otherNumber) {
+			return otherNumber + " is bigger than " + number;
+		}
+		return "Values are equal.";
+	}
 }
