e928fa78a7643204184d893e373e0cd10fecb36d
Jason Zheng <zzheng3@stuy.edu>
2018-10-15 23:25:19 -0400

    added getters for numerator and denominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -3,4 +3,10 @@ public class RationalNumber extends RealNumber {
 	private int denominator;
 	public RationalNumber(int numerator, int denominator) {
 	}
+	public int getNumerator() {
+		return numerator;
+	}
+	public int getDenominator() {
+		return denominator;
+	}
 }
